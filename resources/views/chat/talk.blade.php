7e4c5fec90523c3663cb8c78851b705373295828
takishun0326 <takishun.st3260@gmail.com>
2023-06-23 15:03:19 +0900

    relayout message view

--- resources/views/chat/talk.blade.php
+++ resources/views/chat/talk.blade.php
@@ -13,12 +13,20 @@
         <div class="talk-message-container">
             @foreach($messages_prop as $message_prop)
             <div class="message-container">
+                <!-- パートナー側 -->
                 @if($message_prop->is_user_send === 1)
+                <div class="chat-icon-box">
+                    <div class="chat-icon"></div>
+                </div>
                 <div class="message-box-partner">
                     {{ $message_prop->message }}
                 </div>
+                <!-- ユーザー側 -->
                 @elseif($message_prop->is_user_send === 0)
-                <div class="message-box-user">
+                <div class="chat-icon-box">
+                    <div class="chat-icon right"></div>
+                </div>
+                <div class="message-box-user right">
                     {{ $message_prop->message}}
                 </div>
                 @endif
